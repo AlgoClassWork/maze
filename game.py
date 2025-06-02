59b3e33d3a0bc3e1c7ee649247281ac145d92b1c
AlgoClassWork <kyxec.play@gmail.com>
2025-06-02 14:37:11 +0600

    Add files via upload

--- game.py
+++ game.py
@@ -1,5 +1,21 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    # Свойства наших персонажей
+    def __init__(self, img, cord_x, cord_y, width, height):
+        self.image = transform.scale( image.load(img), (width, height) )
+        self.rect = self.image.get_rect()
+        self.rect.x = cord_x
+        self.rect.y = cord_y
+    # Способность отображатся
+    def show(self):
+        window.blit( self.image, (self.rect.x, self.rect.y) )
+
+# Создание персонажей
+hero = GameSprite(img='hero.png', cord_x=0, cord_y=400, width=80, height=100)
+enemy = GameSprite(img='enemy.png', cord_x=600, cord_y=200, width=100, height=80)
+goal = GameSprite(img='goal.png', cord_x=600, cord_y=420, width=100, height=80)
+
 # Создание экрана
 window = display.set_mode( (700, 500) )
 
@@ -12,6 +28,11 @@ while True:
 
     # Заливка фона определенным цветом
     window.fill( (150, 150, 250) )
-    
+
+    # Отображение персонажей
+    hero.show()
+    enemy.show()
+    goal.show()
+
     # Постоянное обновление кадров на нашем экране
     display.update()
