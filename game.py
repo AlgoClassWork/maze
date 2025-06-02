9ebc3271a9191380b2661aa7aa227b8961e3f708
AlgoClassWork <kyxec.play@gmail.com>
2025-06-02 14:00:18 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,17 @@
+from pygame import *
 
+# Создание экрана
+window = display.set_mode( (700, 500) )
+
+# Игровой цикл
+while True:
+    # Обработка нажатия на крестик (выход из игры)
+    for some_event in event.get():
+        if some_event.type == QUIT:
+            exit()
+
+    # Заливка фона определенным цветом
+    window.fill( (150, 150, 250) )
+    
+    # Постоянное обновление кадров на нашем экране
+    display.update()
